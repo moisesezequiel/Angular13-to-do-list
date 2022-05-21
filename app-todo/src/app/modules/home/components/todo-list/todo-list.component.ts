43966d1a8488642423bdb62bc5bf8bef0208c86c
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2022-05-21 15:39:07 +0100

    Method delete one item and delete all itens

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,10 +9,23 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent implements OnInit {
 
   public taskList: Array<TaskList>=[
+    {task:"new Task", checked:true},
+    {task:"Task 2", checked:true}
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public deleteItem(event:number){
+    this.taskList.splice(event,1)
+  }
+
+  public deleteAll(){
+    const confirm = window.confirm("Deseja realmente deletar todos os itens?")
+    if(confirm){
+      this.taskList = [];
+    }
+
+  }
 }
