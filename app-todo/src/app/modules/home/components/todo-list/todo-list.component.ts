5efafb2ccfe20b1efc8b80a187235908d37e8f2f
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2022-05-25 10:08:28 +0100

    Memory in LocalStorage

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,15 +8,21 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList>=[];
+  public taskList: Array<TaskList>= JSON.parse(localStorage.getItem("list") || '[]');
   constructor() { }
 
   ngDoCheck(): void {
-    this.taskList.sort((first,last)=> Number(first.checked)-Number(last.checked))
+    this.setLocalStorage();
     }
 
   public deleteItem(event:number){
     this.taskList.splice(event,1)
+  }
+  public setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort((first,last)=> Number(first.checked)-Number(last.checked))
+      localStorage.setItem("list",JSON.stringify(this.taskList  ))
+    }
   }
 
   public deleteAll(){
@@ -33,7 +39,7 @@ export class TodoListComponent implements DoCheck {
 
   public validationInput(event:string, index:number){
     if(!event.length){
-    const confirm = window.confirm("Tarefa está vazia,deseja deletar");
+    const confirm = window.confirm("Tarefa está vazia,deseja deletar ?");
 
     if(confirm){
       this.deleteItem(index)
